e3c8ff9502e77e228181a92f3c3addcf359d0532
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:13:45 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-rfc-4122",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -18,26 +18,38 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-ascii-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-standard-library-extensions.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-dependency-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-standard-library-extensions.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-dependency-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
             name: "RFC 4122",
             dependencies: [
                 .product(name: "ASCII Primitives", package: "swift-ascii-primitives"),
-                .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"),
+                .product(
+                    name: "Standard Library Extensions",
+                    package: "swift-standard-library-extensions"
+                ),
                 .product(name: "Dependency Primitives", package: "swift-dependency-primitives"),
             ]
         ),
         .testTarget(
             name: "RFC 4122 Tests",
             dependencies: [
-                "RFC 4122",
+                "RFC 4122"
             ],
             exclude: [
-                "RFC_4122.UUID Foundation Comparison Tests.swift",
+                "RFC_4122.UUID Foundation Comparison Tests.swift"
             ]
         ),
     ],
